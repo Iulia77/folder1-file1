d5b733c893486c6ba26eabc7e06f5d71bd893693
iulia danilova <101838205+Iulia77@users.noreply.github.com>
2022-08-15 20:35:34 +0300

    Update app.js

--- APP | BNM/app.js
+++ APP | BNM/app.js
@@ -1,3 +1,5 @@
+const labels = []
+const rates = []
 const getRatesForDate = (date) => {
     const URL = `https://www.bnm.md/ro/official_exchange_rates?get_xml=1&date=${date}`
 
@@ -10,15 +12,56 @@ const getRatesForDate = (date) => {
         let res = xhr.responseText
 
         //parsing XML
-        console.log(res)
+        let xmlParser = new DOMParser()
+
+        let xmlDoc = xmlParser.parseFromString(res, "text/xml") 
+
+        let valute = xmlDoc.querySelector('[ID="47"]')
+
+        //EUR to MDL
+        let value = parseFloat(valute.lastElementChild.innerHTML)
+        //console.log(xmlDoc)
+        //console.log(value)
+        rates.push(value)
     }
  }
+const getRates = (fromDate, toDate) => {
+    for(let date=fromDate; date< toDate; date++) {
+        if(date<10)
+        date='0' + date
+        labels.push(`${date}.08.2022`)
+        getRatesForDate(`${date}.08.2022`)
+     }
+}
+ 
+ getRates(1,31)
 
+ 
 
+ const plotData = ()=> {
+ const data = {
+    labels: labels,
+    datasets: [{
+      label: 'BNM Exchange rates for EUR/MDL',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: rates,
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const ratesChart = new Chart(
+    document.getElementById('ratesChart'),
+    config
+  );
+}
+setTimeout(plotData, 2000)
  /*
-
 js-->getRatesForDate() --> AJAX--> req(127.0.0.01)--->BNM(API server)
                    <-------res(Allow cross Origin)<----
-
-
  */
