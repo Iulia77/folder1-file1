5e1fbd6bc4eb1cede25a2348a90610f813ae3d75
iulia danilova <101838205+Iulia77@users.noreply.github.com>
2022-08-26 21:44:11 +0300

    Create data.mjs

---
+++ simple weather/data.mjs
@@ -0,0 +1,35 @@
+// WEATHER DATA APP
+
+let data_week = [
+    {
+         date: '22-08-2022',
+         temp: 30
+    },
+    {
+        date: '23-08-2022',
+        temp: 29
+    },
+    {
+        date: '24-08-2022',
+        temp: 28
+    },
+    {
+        date: '25-08-2022',
+        temp: 29
+    },
+    {
+        date: '26-08-2022',
+        temp: 26
+    },
+    {
+        date: '27-08-2022',
+        temp: 25
+    },
+    {
+        date: '28-08-2022',
+        temp: 24
+    },
+]
+
+
+export{data_week}
