51066444d687063e09235dd74fb4327bc62b453f
iulia danilova <101838205+Iulia77@users.noreply.github.com>
2022-09-03 21:48:12 +0300

    Update app.mjs

--- Rating APP node.js/app.mjs
+++ Rating APP node.js/app.mjs
@@ -1,2 +1,9 @@
-import {showUsers} from "./utils.mjs"
+import {showUsers, addUser} from "./utils.mjs"
+
+//addUser()
 showUsers()
+
+//HW1: find a way to format the string
+// name : 12 characters, rate 10 characters  .1 precision
+
+//Users John(0)   
