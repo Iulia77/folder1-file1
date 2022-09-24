2d142ca7b52a543b844058281c174942d1afa9a0
iulia danilova <101838205+Iulia77@users.noreply.github.com>
2022-09-24 21:25:20 +0300

    Create component.js

---
+++ Chestionar_React/component.js
@@ -0,0 +1,35 @@
+// 1. Component represent a part of UI and like functions return HTML elements.
+// 2. The component name must start with UPPER CASE letter.
+
+// 3.   React.createElement(MyComponent, {
+  //      property: "value"
+//       });
+
+// 4. Props are arguments passed into React components. (props stands for properties)
+// 5. "C"
+// 6. Functional component is JS function that returns a react element(JSX), in this case we use paired tag.
+
+// 7. function Child(props) { 
+//    return (<div>This is the {props.name} child!)</div>)
+//    }
+
+//    function Parent(props) { 
+//    return (<div>{props.children}</div>)
+//    }
+
+//    main component, rewrite the code here
+//    function App() {
+//      return (
+//        <div>
+//          <Parent
+//             <Child name="One"/>
+//             <Child name="Two"/>
+//             <Child name="Three"/>
+//              />
+//         </div>
+//       )
+//      }
+
+
+
+//8.   ???
